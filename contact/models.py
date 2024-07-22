118a567136b442766bfb30450fe6c58a8324f664
Gabriel <augusto1793bu@gmail.com>
2024-07-22 16:29:25 -0300

    Criando Tabelas e Relacionamentos

--- contact/models.py
+++ contact/models.py
@@ -1,15 +1,33 @@
 from django.db import models
 from django.utils import timezone
 
+class Aula(models.Model):
+    day = models.CharField(max_length=50)
+    hora_ini = models.TimeField()
+    hora_fim = models.TimeField()
+    
+    def __str__(self) -> str:
+        return f'{self.day} {self.hora_ini}'
+
 class Aluno(models.Model):
-    first_name = models.CharField(max_length=50) #Todos Charfield deve passar um max_lenght
+    first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
-    old = models.CharField(max_length=50)
+    old = models.IntegerField()  # Usar IntegerField para idade
+    category = models.CharField(max_length=50)
     phone = models.CharField(max_length=50)
-    login = models.CharField(max_length=50) #nome.sobrenome no cadastro
+    login = models.CharField(max_length=50)
     birthday = models.DateField()
     created_in = models.DateTimeField(default=timezone.now)
     obs = models.TextField(blank=True)
+    show = models.BooleanField(default=True)
+    picture = models.ImageField(blank=True, upload_to='pictures/%Y/%m/%d')
 
     def __str__(self) -> str:
         return f'{self.first_name} {self.last_name}'
+
+class Aluno_Aula(models.Model):
+    aula = models.ForeignKey(Aula, on_delete=models.SET_NULL, null=True)  # Corrigido para permitir NULL
+    aluno = models.ForeignKey(Aluno, on_delete=models.SET_NULL, null=True)  # Corrigido para permitir NULL
+
+    def __str__(self) -> str:
+        return f'{self.aula} {self.aluno}'
--- project/settings.py
+++ project/settings.py
@@ -123,6 +123,11 @@ STATICFILES_DIRS = (
     BASE_DIR / 'base_static',
 )
 
+STATIC_ROOT = BASE_DIR / 'static' #collectStatic
+
+MEDIA_URL = 'media/'  #arquivos enviados pelo usuário
+MEDIA_ROOT = BASE_DIR / 'media'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
