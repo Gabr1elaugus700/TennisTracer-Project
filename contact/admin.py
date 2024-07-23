754b27a1e4665217f1ade750c1f20ba6735f960f
Gabriel <augusto1793bu@gmail.com>
2024-07-23 16:41:37 -0300

    Relacionamento entre tables Aluno - Aula

--- contact/admin.py
+++ contact/admin.py
@@ -4,4 +4,15 @@ from contact import models
 # Register your models here.
 @admin.register(models.Aluno)
 class AlunoAdmin(admin.ModelAdmin):
-    ...
+    list_display = 'id', 'first_name', 'last_name',
+    ordering = 'id',
+
+@admin.register(models.Aula)
+class AulaAdmin(admin.ModelAdmin):
+    list_display = 'id', 'day', 'hora_ini', 'hora_fim',
+    ordering = '-id',
+
+@admin.register(models.Aluno_Aula)
+class Aluno_AulaAdmin(admin.ModelAdmin):
+    list_display = 'id', 'aula', 'aluno',
+    ordering = 'id',
--- contact/models.py
+++ contact/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 class Aula(models.Model):
     day = models.CharField(max_length=50)
@@ -7,13 +8,12 @@ class Aula(models.Model):
     hora_fim = models.TimeField()
     
     def __str__(self) -> str:
-        return f'{self.day} {self.hora_ini}'
+        return f'{self.id} {self.day} {self.hora_ini}'
 
 class Aluno(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     old = models.IntegerField()  # Usar IntegerField para idade
-    category = models.CharField(max_length=50)
     phone = models.CharField(max_length=50)
     login = models.CharField(max_length=50)
     birthday = models.DateField()
@@ -21,13 +21,19 @@ class Aluno(models.Model):
     obs = models.TextField(blank=True)
     show = models.BooleanField(default=True)
     picture = models.ImageField(blank=True, upload_to='pictures/%Y/%m/%d')
+    category = models.CharField(max_length=50, blank=True)
+    owner = models.ForeignKey(
+        User,
+        on_delete=models.SET_NULL,
+        blank=True, null=True
+    )
 
     def __str__(self) -> str:
-        return f'{self.first_name} {self.last_name}'
+        return f'{self.id} {self.first_name} {self.last_name}'
 
 class Aluno_Aula(models.Model):
     aula = models.ForeignKey(Aula, on_delete=models.SET_NULL, null=True)  # Corrigido para permitir NULL
     aluno = models.ForeignKey(Aluno, on_delete=models.SET_NULL, null=True)  # Corrigido para permitir NULL
 
     def __str__(self) -> str:
-        return f'{self.aula} {self.aluno}'
+        return f'{self.id} {self.aula} {self.aluno}'
